ec14bf7c978093962e1e3e1c74502aca1f01a850
benolayinka <bo97@nau.edu>
2014-03-30 19:14:36 -0700

    File Browser
    Added file browser layout, working to pass to file send fragment

--- FTPChatClient_Fork/src/edu/nau/CS477/Fragments/ChatFragment.java
+++ FTPChatClient_Fork/src/edu/nau/CS477/Fragments/ChatFragment.java
@@ -2,12 +2,17 @@ package edu.nau.CS477.Fragments;
 
 
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import edu.nau.CS477.Classes.FileDialog;
+import edu.nau.CS477.Classes.SelectionMode;
 import com.example.android.navigationdrawerexample.R;
 
 import edu.nau.CS477.Contacts.ContactObject;
@@ -18,6 +23,7 @@ import edu.nau.CS477.Contacts.ContactObject;
 public class ChatFragment extends Fragment {
     public static final String MENU_ITEM_NUMBER = "menu_number";
     ContactObject co;
+    public View rootView;
     
     public ChatFragment() {
         
@@ -37,6 +43,37 @@ public class ChatFragment extends Fragment {
         getActivity().setTitle(menuItem);
        
         return rootView;
+    }
+    
+    public void sendFile(View view){ //called when the sendFileButton is pressed
+    	
+    	Intent intent = new Intent(getActivity(), FileDialog.class); //create a File Browser intent
+        //intent.putExtra(FileDialog.START_PATH, "/sdcard");
+        
+        //can user select directories or not
+        //intent.putExtra(FileDialog.CAN_SELECT_DIR, true);
+        
+        //alternatively you can set file filter
+        //intent.putExtra(FileDialog.FORMAT_FILTER, new String[] { "png" });
+		
+		//View rootView = inflater.inflate(R.layout.file_send_fragment_screen_detail,
+		//		container, false);
+		
+		intent.putExtra(FileDialog.SELECTION_MODE, SelectionMode.MODE_OPEN);
+        
+        startActivityForResult(intent, 1);
+    }
+    
+    public void onActivityResult(int requestCode, int resultCode,
+            Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+            	String Path = data.getStringExtra(FileDialog.RESULT_PATH);
+            	((TextView) rootView.findViewById(R.id.textViewFileSend))
+    			.setText(Path);
+            }
+        }
     }
 }
 
